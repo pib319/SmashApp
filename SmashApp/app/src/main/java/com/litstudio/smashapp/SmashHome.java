14793d9a2bd40021333bf75466ec11f6977cbcd5
Andrew Price <anwepr24@gmail.com>
2017-06-30 16:12:36 -0700

    App Birth
    SmashyMcSmashFace

---
+++ SmashApp/app/src/main/java/com/litstudio/smashapp/SmashHome.java
@@ -0,0 +1,13 @@
+package com.litstudio.smashapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmashHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smash_home);
+    }
+}
